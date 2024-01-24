055f469927c002c92307bf9ce51fb4b5b17f8285
ajay-bhagava <ajay.bhagava@gmail.com>
2024-01-24 14:51:15 -0500

    feat: Added neighbors to MapNode

--- apps/backend/src/algorithms/MapNode.ts
+++ apps/backend/src/algorithms/MapNode.ts
@@ -1,4 +1,5 @@
 import { PathOrFileDescriptor, readFileSync } from "fs";
+import MapEdge from "./MapEdge.ts";
 
 export default class MapNode {
   nodeID: string;
@@ -9,7 +10,7 @@ export default class MapNode {
   nodeType: string;
   longName: string;
   shortName: string;
-  // neighbors: Array<string>;
+  neighbors: Array<string>;
 
   private constructor(properties: Array<string>) {
     let xcoord, ycoord;
@@ -27,7 +28,7 @@ export default class MapNode {
     // Have to do the x and y coordinates separately since they are numbers, not strings
     this.xcoord = Number(xcoord);
     this.ycoord = Number(ycoord);
-    // this.neighbors = new Array<string>
+    this.neighbors = new Array<string>();
   }
 
   static readCsv(filename: PathOrFileDescriptor) {
@@ -40,22 +41,18 @@ export default class MapNode {
     return splitLines.map((props) => new MapNode(props));
   }
 
-  // static connectNodes(nodes: Array<MapNode>, edgesFile: PathOrFileDescriptor): Array<MapNode> {
-  //     const input = readFileSync(edgesFile, "utf8");
-  //
-  //     // Split by lines. The first line is the headers, the last line is a blank
-  //     const lines = input.split(/\r?\n/).slice(1, -1);
-  //     lines.forEach((line) => {
-  //         let [edgeID, startNode, endNode] = line.split(",");
-  //         for (let node of nodes) {
-  //             if (node.nodeID === startNode) {
-  //                 node.neighbors.push(endNode)
-  //             }
-  //             if (node.nodeID === endNode) {
-  //                 node.neighbors.push(startNode)
-  //             }
-  //         }
-  //     });
-  //     return nodes
-  // }
+  static connectNodes(
+    nodes: Array<MapNode>,
+    edges: Array<MapEdge>,
+  ): Map<string, MapNode> {
+    const map: Map<string, MapNode> = new Map<string, MapNode>(
+      nodes.map((node) => [node.nodeID, node]),
+    );
+    for (const edge of edges) {
+      // We can safely assume the graph will contain all nodes due to foreign key constraint
+      map.get(edge.startNode)?.neighbors.push(edge.endNode);
+      map.get(edge.endNode)?.neighbors.push(edge.startNode);
+    }
+    return map;
+  }
 }
--- apps/backend/src/seed.ts
+++ apps/backend/src/seed.ts
@@ -2,16 +2,34 @@ import PrismaClient from "./bin/database-connection.ts";
 import MapNode from "./algorithms/MapNode.ts";
 import MapEdge from "./algorithms/MapEdge.ts";
 
-const nodes: MapNode[] = MapNode.readCsv(
-  `${__dirname}/../csvFiles/L1Nodes.csv`,
-);
-const edges: MapEdge[] = MapEdge.readCsv(
-  `${__dirname}/../csvFiles/L1Edges.csv`,
+type MapNodeNoNeighbors = Omit<MapNode, "neighbors">;
+const nodes: MapNodeNoNeighbors[] = MapNode.readCsv("csvFiles/L1Nodes.csv").map(
+  (node: MapNode) => {
+    // Can ignore this warning because we need to get the neighbors by name to omit them
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { neighbors, ...rest } = node;
+    return rest;
+  },
 );
 
+const edges: MapEdge[] = MapEdge.readCsv("csvFiles/L1Edges.csv");
+
+const moreNodes = MapNode.readCsv("csvFiles/L1Nodes.csv");
+console.log(MapNode.connectNodes(moreNodes, edges));
 export default async function seed(): Promise<void> {
   await PrismaClient.mapEdge.deleteMany();
   await PrismaClient.mapNode.deleteMany();
   await PrismaClient.mapNode.createMany({ data: nodes });
   await PrismaClient.mapEdge.createMany({ data: edges });
 }
+
+PrismaClient.highScore
+  .create({
+    data: {
+      time: new Date().toISOString(),
+      score: 120,
+    },
+  })
+  .catch((err) => {
+    console.log(`Couldn't log due to ${err}`);
+  });
