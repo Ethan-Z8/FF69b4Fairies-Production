362f21e49f862ea13f1c36ca099f00820ec75594
z-altheimer <zmaltheimer@gmail.com>
2024-01-24 19:39:50 -0500

    styled LoginText.css and added Back to Map link

--- apps/frontend/src/components/LoginText.tsx
+++ apps/frontend/src/components/LoginText.tsx
@@ -5,14 +5,14 @@ export function LoginText() {
     <div>
       <div className={"loginTitleText"}>
         <h1>For Admin and Staff Only</h1>
-        <h1>Not a Staff or Admin?</h1>
+        <h2>Not a Staff or Admin?</h2>
+      </div>
+      <div className={"BacktoMap"}>
+        <a href={"/"}>Back to Map</a>
       </div>
       <div className={"loginFor"}>
         <h2>Login for </h2>
         <h3>Brigham and Women's Hospital</h3>
-      </div>
-      <div className={"idNumberEntryText"}>
-        <h1>ID Number</h1> <h2>Swipe card for instant access</h2>
       </div>
     </div>
   );
--- apps/frontend/src/routes/loginPage.tsx
+++ apps/frontend/src/routes/loginPage.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
-import { ExampleComponent } from "../components/ExampleComponent.tsx";
+import { LoginText } from "../components/LoginText.tsx";
+
 function LoginPage() {
   useEffect(() => {
     document.title = "login page";
@@ -7,8 +8,7 @@ function LoginPage() {
   });
   return (
     <div className="w-100 h-100 d-flex flex-column overflow-auto">
-      <h1>This is the login page.</h1>
-      <ExampleComponent></ExampleComponent>
+      <LoginText />
     </div>
   );
 }
