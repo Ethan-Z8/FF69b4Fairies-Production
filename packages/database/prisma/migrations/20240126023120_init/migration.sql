eed55ffbf435f360e75dbba5e4a00cd1912d1e66
ajay-bhagava <ajay.bhagava@gmail.com>
2024-01-25 21:32:19 -0500

    fix: deleted old prisma migrations and remigrated

--- packages/database/prisma/migrations/20240126023120_init/migration.sql
+++ packages/database/prisma/migrations/20240126023120_init/migration.sql
@@ -1,8 +1,8 @@
 -- CreateTable
 CREATE TABLE "MapNode" (
     "nodeID" TEXT NOT NULL,
-    "xcoord" INTEGER NOT NULL,
-    "ycoord" INTEGER NOT NULL,
+    "xcoord" DOUBLE PRECISION NOT NULL,
+    "ycoord" DOUBLE PRECISION NOT NULL,
     "floor" TEXT NOT NULL,
     "building" TEXT NOT NULL,
     "nodeType" TEXT NOT NULL,
@@ -15,10 +15,23 @@ CREATE TABLE "MapNode" (
 -- CreateTable
 CREATE TABLE "MapEdge" (
     "edgeID" TEXT NOT NULL,
+    "startNode" TEXT NOT NULL,
     "endNode" TEXT NOT NULL,
 
     CONSTRAINT "MapEdge_pkey" PRIMARY KEY ("edgeID")
 );
+
+-- CreateTable
+CREATE TABLE "HighScore" (
+    "id" SERIAL NOT NULL,
+    "time" TIMESTAMP(3) NOT NULL,
+    "score" INTEGER NOT NULL,
+
+    CONSTRAINT "HighScore_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "MapEdge" ADD CONSTRAINT "MapEdge_startNode_fkey" FOREIGN KEY ("startNode") REFERENCES "MapNode"("nodeID") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "MapEdge" ADD CONSTRAINT "MapEdge_endNode_fkey" FOREIGN KEY ("endNode") REFERENCES "MapNode"("nodeID") ON DELETE RESTRICT ON UPDATE CASCADE;
