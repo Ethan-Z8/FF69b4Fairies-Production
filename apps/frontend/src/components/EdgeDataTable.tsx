9e67589aa197f7d926c63bad80af0a2ea213b231
z-altheimer <zmaltheimer@gmail.com>
2024-01-29 21:41:47 -0500

    created node and edge table components, and created universal datatable styling. map data route not currently working :(

---
+++ apps/frontend/src/components/EdgeDataTable.tsx
@@ -0,0 +1,14 @@
+export function EdgeDataTable() {
+  return (
+    <div className={"DataTable"}>
+      <h2>Current Edge Data</h2>
+      <table>
+        <tr>
+          <th>edgeID</th>
+          <th>startNode</th>
+          <th>endNode</th>
+        </tr>
+      </table>
+    </div>
+  );
+}
