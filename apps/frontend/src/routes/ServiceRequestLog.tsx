b0b5e38debe5322b8f126e4bbf85a40dcccb8787
J-Munny144 <94010822+J-Munny144@users.noreply.github.com>
2024-01-29 21:56:59 -0500

    Save changes

---
+++ apps/frontend/src/routes/ServiceRequestLog.tsx
@@ -0,0 +1,30 @@
+import React, { useEffect } from "react";
+
+import "../style/ServiceRequestLog.css";
+
+import { BackButton } from "../components/BackButton.tsx";
+
+//Stores all service requests entered into the system
+function ServiceRequestLog() {
+  useEffect(() => {
+    document.title = "Service Request page";
+    console.log(`rendered component`);
+  });
+
+  return (
+    <div className={"ServiceTable"}>
+      <BackButton />
+
+      <table>
+        <tr>
+          <th>Doctor John</th>
+          <th>Doctor Rahim</th>
+          <th>Doctor Anderson</th>
+          <th>Doctor Bob</th>
+        </tr>
+      </table>
+    </div>
+  );
+}
+
+export { ServiceRequestLog };
