bc75a67316fed49331d2b0ad711c5645527e9081
z-altheimer <zmaltheimer@gmail.com>
2024-01-31 20:38:29 -0500

    fetch request was changed to axios get

--- apps/frontend/src/components/NodeDataTable.tsx
+++ apps/frontend/src/components/NodeDataTable.tsx
@@ -1,5 +1,22 @@
 import "../styling/DataTable.css";
+import axios from "axios";
+import { useEffect } from "react";
+
+async function getNodes() {
+  try {
+    const allNodes = await axios.get("/api/map");
+    const nodesData: Node[] = Object.values(allNodes.data);
+    console.log(nodesData);
+  } catch (error) {
+    console.error("Error fetching map nodes", error);
+  }
+}
+
 export function NodeDataTable() {
+  useEffect(() => {
+    getNodes();
+  }, []);
+
   return (
     <div className={"DataTable"}>
       <h2>Current Node Data</h2>
@@ -13,27 +30,10 @@ export function NodeDataTable() {
           <th>nodeType</th>
           <th>longName</th>
           <th>shortName</th>
+          <th>neighbors</th>
         </tr>
-        <tr>
-          <td>node1</td>
-          <td>0</td>
-          <td>0</td>
-          <td>first floor</td>
-          <td>hospital</td>
-          <td>bad</td>
-          <td>longername</td>
-          <td>shortname</td>
-        </tr>
-        <tr>
-          <td>node1</td>
-          <td>0</td>
-          <td>0</td>
-          <td>first floor</td>
-          <td>hospital</td>
-          <td>bad</td>
-          <td>longername</td>
-          <td>shortname</td>
-        </tr>
+
+        <tr></tr>
       </table>
     </div>
   );
