310e3f5aac7d4b521c51c3b285f07343a5975251
ajay-bhagava <ajay.bhagava@gmail.com>
2024-02-01 21:15:48 -0500

    JSDoc for the serviceRequest creation endpoint

--- apps/backend/src/routes/serviceRequest.ts
+++ apps/backend/src/routes/serviceRequest.ts
@@ -5,6 +5,15 @@ import { Prisma } from "database";
 
 export const serviceRouter: Router = express.Router();
 
+/**
+ * create a service request. For now, the location is a string
+ * the request body should contain 3 fields:
+ * {
+ *     typeService: string,
+ *     reason: string,
+ *     nodeLoc: string
+ * }
+ */
 serviceRouter.post("/", async (req: Request, res: Response) => {
   const serviceRequest: Prisma.ServiceRequestCreateInput = req.body;
   await insertServiceRequest(
