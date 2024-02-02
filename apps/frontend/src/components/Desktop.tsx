840bea6cca9595026e8601a9dd23582a13b9415f
ajay-bhagava <ajay.bhagava@gmail.com>
2024-02-02 00:31:49 -0500

    FIxed navigation
    Ability to navigate to service request menu, service request logs, map nodes table, and csv upload

--- apps/frontend/src/components/Desktop.tsx
+++ apps/frontend/src/components/Desktop.tsx
@@ -1,12 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { SelectorTabs } from "./SelectorTabs.tsx";
 import Map from "../components/Map.tsx";
-import GroundFloorMapPath from "../assets/hospitalmaps/00_thegroundfloor.png";
+// import GroundFloorMapPath from "../assets/hospitalmaps/00_thegroundfloor.png";
 import LL1MapPath from "../assets/hospitalmaps/00_thelowerlevel1.png";
-import LL2MapPath from "../assets/hospitalmaps/00_thelowerlevel2.png";
-import FirstFloorMapPath from "../assets/hospitalmaps/01_thefirstfloor.png";
-import SecondFloorMapPath from "../assets/hospitalmaps/02_thesecondfloor.png";
-import ThirdFloorMapPath from "../assets/hospitalmaps/03_thethirdfloor.png";
+// import LL2MapPath from "../assets/hospitalmaps/00_thelowerlevel2.png";
+// import FirstFloorMapPath from "../assets/hospitalmaps/01_thefirstfloor.png";
+// import SecondFloorMapPath from "../assets/hospitalmaps/02_thesecondfloor.png";
+// import ThirdFloorMapPath from "../assets/hospitalmaps/03_thethirdfloor.png";
 import TransformContainer from "../components/TransformContainer.tsx";
 import PathFindingForm from "./PathFindingForm.tsx";
 import { DisplayPath } from "./DisplayPath.tsx";
@@ -29,34 +30,25 @@ export const Desktop = ({
 }: menuProps) => {
   const [pathStart, setPathStart] = useState<string>("");
   const [pathEnd, setPathEnd] = useState<string>("");
-  let mapPath = LL1MapPath;
+  const mapPath = LL1MapPath;
   const handleFormSubmit = () => {
     setPathStart(pathStart);
     setPathEnd(pathEnd);
   };
+  const navigate = useNavigate();
 
-  if (selectedTab === 0) {
-    mapPath = GroundFloorMapPath;
-  }
-
-  if (selectedTab === 1) {
-    mapPath = LL1MapPath;
-  }
-
-  if (selectedTab === 2) {
-    mapPath = LL2MapPath;
-  }
-
-  if (selectedTab === 3) {
-    mapPath = FirstFloorMapPath;
-  }
-
-  if (selectedTab === 4) {
-    mapPath = SecondFloorMapPath;
-  }
-
-  if (selectedTab === 5) {
-    mapPath = ThirdFloorMapPath;
+  switch (selectedTab) {
+    case 0:
+      navigate("/mapdata");
+      break;
+    case 1:
+      navigate("/file_reader");
+      break;
+    case 5:
+      navigate("/requestLogPage");
+      break;
+    default:
+      break;
   }
 
   async function getMapNodes() {
