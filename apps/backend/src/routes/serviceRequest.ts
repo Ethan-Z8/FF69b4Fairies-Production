329eaa52c9e870e3841462018b9e5f05950f7076
ajay-bhagava <ajay.bhagava@gmail.com>
2024-02-05 22:27:55 -0500

    Fixed create service Request and create Employee
    create service request takes DisplayName as foreign key, not username

--- apps/backend/src/routes/serviceRequest.ts
+++ apps/backend/src/routes/serviceRequest.ts
@@ -1,57 +1,20 @@
 import express, { Request, Response, Router } from "express";
 import prisma from "../bin/database-connection.ts";
-import { insertServiceRequest } from "database/src/Requests.ts";
 import { Prisma } from "database";
 
 const router: Router = express.Router();
 
 router.post("/create", async (req: Request, res: Response) => {
-  const serviceRequest: Prisma.ServiceRequestCreateInput = req.body;
-  // const serviceRequest = req.body as Prisma.ServiceRequestCreateManyInput[];
-  //     await insertServiceRequest(serviceRequest);
-  //     prisma.serviceRequest.createMany({
-  //         data: serviceRequest,
-  //         skipDuplicates: true
-  //     }).then(() => res.status(200)).catch(() => res.status(400));
-  // });for(request : Service)
-
-  // let employees = ["jon", "Monty"];
-  // for(let i= 0; i<employees.length; i++) {
-  //     const employeeInput: { connect: { displayName: string } } = {
-  //         connect: {
-  //             displayName: employees[i],
-  //         },
-  //     };
-
+  const serviceRequest = req.body as Prisma.ServiceRequestCreateManyInput;
   try {
-    // Fetch all employee display names from the database
-    const employees = await prisma.employee.findMany({
-      select: {
-        displayName: true,
-      },
+    await prisma.serviceRequest.createMany({
+      data: serviceRequest,
+      skipDuplicates: true,
     });
-    const employeeDisplayNames: string[] = employees.map(
-      (employee) => employee.displayName,
-    );
-    for (const displayName of employeeDisplayNames) {
-      const employeeInput: { connect: { displayName: string } } = {
-        connect: {
-          displayName,
-        },
-      };
-
-      await insertServiceRequest(
-        serviceRequest.typeService,
-        serviceRequest.reason,
-        serviceRequest.nodeLoc,
-        serviceRequest.progress,
-        employeeInput,
-      );
-      res.status(200).send("Successfully Inserted");
-    }
+    res.sendStatus(200);
   } catch (e) {
-    console.error("Error creating service requests:", e);
-    res.status(500).send("Internal Server Error");
+    console.log((e as Error).message);
+    res.sendStatus(400);
   }
 });
 
