0258ccbc0e37d4e4a8d6d865897ea090c04fe8c8
J-Munny144 <94010822+J-Munny144@users.noreply.github.com>
2024-02-12 21:33:30 -0500

    Added a label to the specialist dropdown menu

--- apps/frontend/src/components/MaintenanceRequestFields.tsx
+++ apps/frontend/src/components/MaintenanceRequestFields.tsx
@@ -1,22 +1,30 @@
-import { TextField, Select, MenuItem } from "@mui/material";
+import {
+  TextField,
+  Select,
+  MenuItem,
+  InputLabel,
+  FormControl,
+} from "@mui/material";
 
 export function MaintenanceRequestFields() {
   return (
     <>
       <TextField label="Machine's Issue" name="issue" required multiline />
-
-      <Select
-        label="Specialist Required"
-        labelId="machineLocation"
-        defaultValue="Any"
-        name="machineLocation"
-        required
-      >
-        <MenuItem value="Any">Any</MenuItem>
-        <MenuItem value="Janitor">Janitor</MenuItem>
-        <MenuItem value="Electric">Electric</MenuItem>
-        <MenuItem value="Mechanic">Mechanic</MenuItem>
-      </Select>
+      <FormControl>
+        <InputLabel>Type of Employee</InputLabel>
+        <Select
+          label="Specialist Required"
+          labelId="specialist"
+          defaultValue="Any"
+          name="specialist"
+          required
+        >
+          <MenuItem value="Any">Any</MenuItem>
+          <MenuItem value="Janitor">Janitor</MenuItem>
+          <MenuItem value="Electric">Electric</MenuItem>
+          <MenuItem value="Mechanic">Mechanic</MenuItem>
+        </Select>
+      </FormControl>
     </>
   );
 }
