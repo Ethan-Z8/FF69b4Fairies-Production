687acd4944be19e6afa98a862bbea94d9e145a7a
ajay-bhagava <ajay.bhagava@gmail.com>
2024-02-14 22:31:25 -0500

    condensed navbar

--- apps/frontend/src/components/Navigation.tsx
+++ apps/frontend/src/components/Navigation.tsx
@@ -2,8 +2,9 @@ import { useAuth0 } from "@auth0/auth0-react";
 import { useEffect } from "react";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-// import logo from "../assets/logo.svg";
-import image1 from "../assets/image-1.png";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import logo from "../assets/image-1.png";
+import "../styling/Navigation.css";
 
 export function Navigation() {
   const { loginWithRedirect, logout } = useAuth0();
@@ -50,7 +51,7 @@ export function Navigation() {
       <Navbar.Brand>
         <Nav.Link href="/">
           <img
-            src={image1}
+            src={logo}
             alt="Logo"
             style={{
               maxHeight: "4rem",
@@ -60,31 +61,32 @@ export function Navigation() {
           />
         </Nav.Link>
       </Navbar.Brand>
-      <Nav className="d-flex justify-content-bewteen">
+      <Nav className="d-flex justify-content-around w-50">
         <Nav.Link style={{ color: "white" }} href="/">
           Home Page
         </Nav.Link>
-        <Nav.Link style={{ color: "white" }} href="/createServiceRequest">
-          Create Service Request
-        </Nav.Link>
-        <Nav.Link style={{ color: "white" }} href="/viewServiceRequest">
-          View Service Requests
-        </Nav.Link>
-        <Nav.Link style={{ color: "white" }} href="/importAndExportData">
-          Import & Export Data
-        </Nav.Link>
-        <Nav.Link style={{ color: "white" }} href="/mapData">
-          View Map Data
-        </Nav.Link>
+        <NavDropdown
+          title={<span style={{ color: "white" }}>Service Requests</span>}
+          className="navbar-item"
+        >
+          <NavDropdown.Item href="/createServiceRequest">
+            Create Service Request
+          </NavDropdown.Item>
+          <NavDropdown.Item href="/viewServiceRequest">
+            View Service Requests
+          </NavDropdown.Item>
+        </NavDropdown>
+        <NavDropdown title={<span style={{ color: "white" }}>Map Data</span>}>
+          <Nav.Link href="/importAndExportData">Import & Export Data</Nav.Link>
+          <Nav.Link href="/mapData">View Map Data</Nav.Link>
+        </NavDropdown>
         {isAuthenticated && (
-          <>
-            <Nav.Link style={{ color: "white" }} href="/addEmployee">
-              Add Employee
-            </Nav.Link>
-            <Nav.Link style={{ color: "white" }} href="/viewEmployeeData">
-              View Employees
-            </Nav.Link>
-          </>
+          <NavDropdown
+            title={<span style={{ color: "white" }}>Employee Data</span>}
+          >
+            <Nav.Link href="/addEmployee">Add Employee</Nav.Link>
+            <Nav.Link href="/viewEmployeeData">View Employees</Nav.Link>
+          </NavDropdown>
         )}
       </Nav>
       {!isAuthenticated && (
