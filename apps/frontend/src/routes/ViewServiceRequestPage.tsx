26f99d0a72993fdc2a3c364c85ef424cf79eb196
z-altheimer <zmaltheimer@gmail.com>
2024-02-19 22:21:14 -0500

    added service request boxes for dashboard

--- apps/frontend/src/routes/ViewServiceRequestPage.tsx
+++ apps/frontend/src/routes/ViewServiceRequestPage.tsx
@@ -17,6 +17,7 @@ import {
 } from "@mui/material";
 import { ServiceRequestRow } from "../components/ServiceRequestRow.tsx";
 import { ServiceRequestType } from "common/src/interfaces/ServiceRequest.ts";
+import "../styling/viewRequestPage.css";
 
 export function ViewServiceRequestPage() {
   const [loaded, setLoaded] = useState<boolean>(false);
@@ -53,115 +54,144 @@ export function ViewServiceRequestPage() {
 
   return (
     loaded && (
-      <Paper
-        elevation={24}
-        sx={{
-          my: 8,
-          mx: "auto",
-          display: "flex",
-          flexDirection: "column",
-          gap: "1rem",
-          width: "90%",
-          border: "8px solid #012D5A",
-          borderRadius: "8px",
-          padding: "1rem",
-          margin: "1rem",
-        }}
-      >
-        <Typography variant="h4">Service Requests</Typography>
-        <Box sx={{ display: "flex", gap: 2, width: "53%" }}>
-          <FormControl sx={{ flex: 1 }}>
-            <InputLabel>Progress Filter</InputLabel>
-            <Select
-              value={statusFilter}
-              label="Status Filter"
-              id="status"
-              onChange={(e) => setStatusFilter(e.target.value)}
-            >
-              <MenuItem value="">Any</MenuItem>
-              <MenuItem value="Unassigned">Unassigned</MenuItem>
-              <MenuItem value="Assigned">Assigned</MenuItem>
-              <MenuItem value="InProgress">In Progress</MenuItem>
-              <MenuItem value="Completed">Completed</MenuItem>
-            </Select>
-          </FormControl>
-          <FormControl sx={{ flex: 1 }}>
-            <InputLabel id="typeServiceLabel">Service Type Filter</InputLabel>
-            <Select
-              value={typeServiceFilter}
-              label="Service Type Filter"
-              id="type"
-              onChange={(e) => setTypeServiceFilter(e.target.value)}
-            >
-              <MenuItem value=""> Any</MenuItem>
-              <MenuItem value="Maintenance">Maintenance</MenuItem>
-              <MenuItem value="Religious">Religious</MenuItem>
-              <MenuItem value="Sanitation">Sanitation</MenuItem>
-              <MenuItem value="Flowers">Flowers</MenuItem>
-              <MenuItem value="InternalTransportation">
-                Internal Transportation
-              </MenuItem>
-            </Select>
-          </FormControl>
-          <FormControl sx={{ flex: 1 }}>
-            <InputLabel>Employee Filter</InputLabel>
-            <Select
-              value={employeeFilter}
-              label="Employee Filter"
-              id="employee"
-              onChange={(e) => setEmployeeFilter(e.target.value)}
-            >
-              <MenuItem value="">Any</MenuItem>
-              {employees.map((employee) => (
-                <MenuItem key={employee.username} value={employee.username}>
-                  {employee.displayName}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-        </Box>
-        <TableContainer
-          sx={{ border: 1, borderColor: "#44444444", borderRadius: 2 }}
+      <div className={"topContainer"}>
+        <Paper
+          elevation={24}
+          sx={{
+            my: 8,
+            mx: "auto",
+            display: "flex",
+            flexDirection: "column",
+            gap: "1rem",
+            width: "90%",
+            border: "8px solid #012D5A",
+            borderRadius: "8px",
+            padding: "1rem",
+            margin: "1rem",
+          }}
         >
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell />
-                <TableCell>Date</TableCell>
-                <TableCell>Type Of Service</TableCell>
-                <TableCell>Location</TableCell>
-                <TableCell>Employee</TableCell>
-                <TableCell>Progress</TableCell>
-                <TableCell>Priority</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {serviceRequests
-                .filter((req) => {
-                  if (statusFilter !== "") {
-                    return req.progress === statusFilter;
-                  }
-                  return true;
-                })
-                .filter((req) => {
-                  if (typeServiceFilter !== "") {
-                    return req.typeService === typeServiceFilter;
-                  }
-                  return true;
-                })
-                .filter((req) => {
-                  if (employeeFilter !== "") {
-                    return req.employee === employeeFilter;
-                  }
-                  return true;
-                })
-                .map((row) => {
-                  return <ServiceRequestRow {...row} />;
-                })}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </Paper>
+          <div className={"dashboard"}>
+            <div className={"dashboardLeft"}>
+              <div className={"topRow"}>
+                <div className={"text"}>
+                  <h3>Unassigned</h3>
+                </div>
+                <div className={"text"}>
+                  <h3>Assigned</h3>
+                </div>
+              </div>
+              <div className={"bottomRow"}>
+                <div className={"rowLeft"}>
+                  <div className={"text"}>
+                    <h3>In Progress</h3>
+                  </div>
+                </div>
+                <div className={"rowRight"}>
+                  <div className={"text"}>
+                    <h3>Completed</h3>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={"dashboardRight"}>
+              <h2>Service Request Chart</h2>
+            </div>
+          </div>
+          <Typography variant="h4">Service Requests</Typography>
+          <Box sx={{ display: "flex", gap: 2, width: "53%" }}>
+            <FormControl sx={{ flex: 1 }}>
+              <InputLabel>Progress Filter</InputLabel>
+              <Select
+                value={statusFilter}
+                label="Status Filter"
+                id="status"
+                onChange={(e) => setStatusFilter(e.target.value)}
+              >
+                <MenuItem value="">Any</MenuItem>
+                <MenuItem value="Unassigned">Unassigned</MenuItem>
+                <MenuItem value="Assigned">Assigned</MenuItem>
+                <MenuItem value="InProgress">In Progress</MenuItem>
+                <MenuItem value="Completed">Completed</MenuItem>
+              </Select>
+            </FormControl>
+            <FormControl sx={{ flex: 1 }}>
+              <InputLabel id="typeServiceLabel">Service Type Filter</InputLabel>
+              <Select
+                value={typeServiceFilter}
+                label="Service Type Filter"
+                id="type"
+                onChange={(e) => setTypeServiceFilter(e.target.value)}
+              >
+                <MenuItem value=""> Any</MenuItem>
+                <MenuItem value="Maintenance">Maintenance</MenuItem>
+                <MenuItem value="Religious">Religious</MenuItem>
+                <MenuItem value="Sanitation">Sanitation</MenuItem>
+                <MenuItem value="Flowers">Flowers</MenuItem>
+                <MenuItem value="InternalTransportation">
+                  Internal Transportation
+                </MenuItem>
+              </Select>
+            </FormControl>
+            <FormControl sx={{ flex: 1 }}>
+              <InputLabel>Employee Filter</InputLabel>
+              <Select
+                value={employeeFilter}
+                label="Employee Filter"
+                id="employee"
+                onChange={(e) => setEmployeeFilter(e.target.value)}
+              >
+                <MenuItem value="">Any</MenuItem>
+                {employees.map((employee) => (
+                  <MenuItem key={employee.username} value={employee.username}>
+                    {employee.displayName}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          </Box>
+          <TableContainer
+            sx={{ border: 1, borderColor: "#44444444", borderRadius: 2 }}
+          >
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell />
+                  <TableCell>Date</TableCell>
+                  <TableCell>Type Of Service</TableCell>
+                  <TableCell>Location</TableCell>
+                  <TableCell>Employee</TableCell>
+                  <TableCell>Progress</TableCell>
+                  <TableCell>Priority</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {serviceRequests
+                  .filter((req) => {
+                    if (statusFilter !== "") {
+                      return req.progress === statusFilter;
+                    }
+                    return true;
+                  })
+                  .filter((req) => {
+                    if (typeServiceFilter !== "") {
+                      return req.typeService === typeServiceFilter;
+                    }
+                    return true;
+                  })
+                  .filter((req) => {
+                    if (employeeFilter !== "") {
+                      return req.employee === employeeFilter;
+                    }
+                    return true;
+                  })
+                  .map((row) => {
+                    return <ServiceRequestRow {...row} />;
+                  })}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Paper>
+      </div>
     )
   );
 }
