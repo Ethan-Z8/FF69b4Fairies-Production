664f13686e50d14cffaff5deb0ad106fb6cc2583
Ethan Z <ezhong888@gmail.com>
2024-02-26 13:18:42 -0500

    trying to get css styling creating path

--- apps/frontend/src/routes/AboutMe.tsx
+++ apps/frontend/src/routes/AboutMe.tsx
@@ -1,6 +1,52 @@
 //import React, { useEffect, useState } from "react";
 
 const AboutMe = () => {
-  return;
+  return (
+    <div className="about-the-team">
+      <h2>Our Team</h2>
+      <div className="team-members-flex">
+        <div className="row">
+          <div className="team-member">
+            <h3>mem1</h3>
+            <p>Softeng</p>
+            <p>biotest</p>
+          </div>
+          <div className="team-member">
+            <h3>mem2</h3>
+            <p>Softeng</p>
+            <p>biotest</p>
+          </div>
+
+          <div className="team-member">
+            <h3>mem3</h3>
+            <p>Softeng</p>
+            <p>biotest</p>
+          </div>
+          <div className="team-member">
+            <h3> mem4</h3>
+            <p>Softeng</p>
+            <p>biotest</p>
+          </div>
+          <div className="team-member">
+            <h3>mem5</h3>
+            <p>Softeng</p>
+            <p>biotest</p>
+          </div>
+        </div>
+
+        <div className="team-member">
+          <h3>mem6</h3>
+          <p>Softeng</p>
+          <p>biotest</p>
+        </div>
+
+        <div className="team-member">
+          <h3>mem7</h3>
+          <p>Softeng</p>
+          <p>biotest</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default AboutMe;
