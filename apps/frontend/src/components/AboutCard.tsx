278bb786496dc462b0fd342bed0abb2b79cacce7
Ethan Z <ezhong888@gmail.com>
2024-02-27 21:00:13 -0500

    added an on hover put in my quote

--- apps/frontend/src/components/AboutCard.tsx
+++ apps/frontend/src/components/AboutCard.tsx
@@ -1,4 +1,5 @@
 import {
+  Box,
   Button,
   Card,
   CardActions,
@@ -6,6 +7,20 @@ import {
   CardMedia,
   Typography,
 } from "@mui/material";
+import Modal from "@mui/material/Modal";
+import React from "react";
+
+const style = {
+  position: "absolute" as const,
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "0px solid #000 !important",
+  boxShadow: 24,
+  p: 4,
+};
 
 interface AboutInfo {
   Name: string;
@@ -19,6 +34,7 @@ interface AboutInfo {
   Imagepath: string;
   Favorite_quote: string;
 }
+
 const AboutCard = ({
   Name,
   role,
@@ -31,18 +47,51 @@ const AboutCard = ({
   Imagepath,
   Favorite_quote,
 }: AboutInfo) => {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
     <Card
-      sx={{ maxWidth: 700 }}
+      sx={{
+        maxWidth: 700,
+        "&:hover": {
+          //width: "100%",
+          backgroundColor: "#e9e8e9",
+          // backgroundColor:"green",
+        },
+      }}
       style={{
         paddingTop: "10px",
         paddingRight: "10px",
         paddingLeft: "10px",
-        maxWidth: "18%",
+        maxWidth: "19%",
       }}
     >
-      <CardMedia sx={{ height: 250 }} image={Imagepath} title={Name} />
-      <CardContent>
+      <CardMedia
+        sx={{ height: 250 }}
+        image={Imagepath}
+        title={Name}
+        onClick={handleOpen}
+      />
+
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Favorite Quote:
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            {Favorite_quote}
+          </Typography>
+        </Box>
+      </Modal>
+
+      <CardContent onClick={handleOpen}>
         <Typography gutterBottom variant="h5" component="div" maxWidth={"50%"}>
           {Name}
         </Typography>
@@ -61,20 +110,20 @@ const AboutCard = ({
         <Typography variant="body2" color="text.secondary">
           Email: {Email}
         </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Favorite quote: {Favorite_quote}
-        </Typography>
       </CardContent>
       <CardActions>
         <Button size="small" variant="outlined" href={Github}>
           Github
         </Button>
         <Button size="small" href={Linkdin}>
-          {" "}
           Linkdin{" "}
         </Button>
       </CardActions>
     </Card>
+
+    // <div style={{backgroundColor = "yellow"}}>
+    // </div>
+    // <Paper/>
   );
 };
 export default AboutCard;
