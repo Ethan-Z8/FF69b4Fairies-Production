4b4db55edfaabdbebc6ebbd9c24232291572faca
J-Munny144 <94010822+J-Munny144@users.noreply.github.com>
2024-02-27 22:27:02 -0500

    Prototyped implementation of Joe and Wong cards

--- apps/frontend/src/routes/AboutMe.tsx
+++ apps/frontend/src/routes/AboutMe.tsx
@@ -1,6 +1,8 @@
 //import React, { useEffect, useState } from "react";
 
 import AboutCard from "../components/AboutCard.tsx";
+import Wong from "../assets/Wong.png";
+import Joe from "../assets/Joseph-SA.png";
 import Aish from "../../src/assets/Aishwarya.png";
 import Ajay from "../../src/assets/Ajay.png";
 import Ethan from "../../src/assets/Ethan.png";
@@ -23,12 +25,12 @@ const AboutMe = () => {
         gap: "10px",
       }}
     >
-      <h5 style={{ textAlign: "center" }}>
+      <h2 style={{ textAlign: "center" }}>
         Our sincerest gratitude towards Brigham and Women’s Hospital and their
         representative, Mr. Andrew Shinn.
-      </h5>
-      <h1 style={{ textAlign: "center" }}>Our Team</h1>
-      <div className="team-members-flex">
+      </h2>
+      <h1 style={{ textAlign: "center", paddingTop: "50px" }}>Faculty</h1>
+      <div className="faculty-flex">
         <div
           style={{
             display: "flex",
@@ -38,7 +40,7 @@ const AboutMe = () => {
           }}
         >
           <AboutCard
-            Name={"Ajay Bhagavatula"}
+            Name={"Wilson Wong"}
             role={"Team Lead"}
             devrole={"FullStack Engineer"}
             ClassYear={"2026"}
@@ -46,143 +48,180 @@ const AboutMe = () => {
             Email={"asdf@wpi.edu"}
             Github={"google.com"}
             Linkdin={"asdf.com"}
-            Imagepath={Ajay}
-            Favorite_quote={""}
-          />
-
-          <AboutCard
-            Name={"Leo Hirano"}
-            role={"Frontend Lead"}
-            devrole={"FullStack Engineer"}
-            ClassYear={"2025"}
-            Major={"Computer Science"}
-            Email={"lkhirano@wpi.edu"}
-            Github={"https://github.com/notLeoHirano"}
-            Linkdin={"asdf.com"}
-            Imagepath={Leo}
-            Favorite_quote={"One million billion marbles"}
-          />
-          <AboutCard
-            Name={"Vaansh Mansharamani"}
-            role={"Assistant Lead"}
-            devrole={"Algorithm Engineer"}
-            ClassYear={"2026"}
-            Major={"Computer Science"}
-            Email={" vmansharamani@wpi.edu"}
-            Github={"https://github.com/JustSomeOrdinaryCardboardBox"}
-            Linkdin={
-              "https://www.linkedin.com/in/vaansh-mansharamani-30a6b7227/"
-            }
-            Imagepath={Vaanch}
+            Imagepath={Wong}
             Favorite_quote={""}
           />
           <AboutCard
-            Name={"Ethan Zhong"}
-            role={"Product Manager"}
-            devrole={"Algorithm Engineer"}
-            ClassYear={"2025"}
-            Major={"Computer Science and Robotics Engineering"}
-            Email={"ezhong@wpi.edu"}
-            Github={"https://github.com/Ethan-Z8"}
-            Linkdin={"https://www.linkedin.com/in/ethan-zhong-76886a21a/"}
-            Imagepath={Ethan}
-            Favorite_quote={
-              "Success is not final, failure is not fatal: it is the courage to continue that counts. - Winston Churchill"
-            }
-          />
-          <AboutCard
-            Name={"Mahit Verma"}
-            role={"Product Owner"}
-            devrole={"Full Stack Engineer"}
-            ClassYear={"2026"}
-            Major={"Computer Science and Data Science"}
-            Email={"mverma@wpi.edu"}
-            Github={"https://github.com/MaVeryo"}
-            Linkdin={"https://www.linkedin.com/in/mahit-verma/"}
-            Imagepath={Mahit}
-            Favorite_quote={""}
-          />
-        </div>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            paddingTop: "20px",
-            paddingBottom: "10px",
-            justifyContent: "space-between",
-          }}
-        >
-          <AboutCard
-            Name={"Zane Altheimer"}
-            role={"Document Analyst"}
-            devrole={"Frontend Engineer"}
+            Name={"Joseph Cardarelli"}
+            role={"Team Coach"}
+            devrole={"N/A"}
             ClassYear={"2024"}
-            Major={"Robotics Engineering"}
-            Email={"zaaltheimer@wpi.edu"}
-            Github={"https://github.com/z-altheimer"}
-            Linkdin={"https://www.linkedin.com/in/zaltheimer/"}
-            Imagepath={Zane}
-            Favorite_quote={
-              "'The machine does not isolate man from the great problems of nature but plunges him more deeply into them.' - Antoine de Saint-Exupéry, The Little Prince"
-            }
-          />
-          <AboutCard
-            Name={"Justin Smith"}
-            role={"Full-time Developer"}
-            devrole={"Frontend Engineer"}
-            ClassYear={"2025"}
-            Major={"Computer Science and Game Development"}
-            Email={"jmsmith2@wpi.edu"}
-            Github={"https://github.com/JustinS-23"}
-            Linkdin={"https://www.linkedin.com/in/justin-smith-b61647215/"}
-            Imagepath={Justin}
-            Favorite_quote={"'Float like a butterfly, sting like a bee.'"}
-          />
-          <AboutCard
-            Name={"Maxwell Friedman"}
-            role={"Scrum Master"}
-            devrole={"Frontend Engineer"}
-            ClassYear={"2025"}
-            Major={"Robotics Engineering"}
-            Email={"mlfriedman@wpi.edu"}
-            Github={" https://github.com/Steelplate"}
-            Linkdin={" https://www.linkedin.com/in/friedman-maxwell/"}
-            Imagepath={Max}
-            Favorite_quote={""}
-          />
-          <AboutCard
-            Name={"Shawn Patel"}
-            role={""}
-            devrole={"Backend Engineer"}
-            ClassYear={"2026"}
-            Major={"Computer Science"}
-            Email={"snpatel@wpi.edu"}
-            Github={"https://github.com/shawnp221"}
-            Linkdin={"https://www.linkedin.com/in/shawn-patel-359a562a7/"}
-            Imagepath={Shawn}
-            Favorite_quote={
-              "'Every man must do two things alone; he must do his own believing and his own dying.' - Martin Luther King Jr"
-            }
-          />
-
-          <AboutCard
-            Name={"Aishwarya Silam"}
-            role={""}
-            devrole={"Backend Engineer"}
-            ClassYear={"2026"}
             Major={"Computer Science"}
             Email={"asdf@wpi.edu"}
             Github={"google.com"}
-            Linkdin={"asdf.com"}
-            Imagepath={Aish}
+            Linkdin={"https://www.linkedin.com/in/joseph-cardarelli-3a12a1254/"}
+            Imagepath={Joe}
             Favorite_quote={""}
           />
         </div>
+        <h1 style={{ textAlign: "center", paddingTop: "100px" }}>Our Team</h1>
+        <div className="team-members-flex">
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              paddingTop: "20px",
+              justifyContent: "space-between",
+            }}
+          >
+            <AboutCard
+              Name={"Ajay Bhagavatula"}
+              role={"Team Lead"}
+              devrole={"FullStack Engineer"}
+              ClassYear={"2026"}
+              Major={"Computer Science"}
+              Email={"asdf@wpi.edu"}
+              Github={"google.com"}
+              Linkdin={"asdf.com"}
+              Imagepath={Ajay}
+              Favorite_quote={""}
+            />
+
+            <AboutCard
+              Name={"Leo Hirano"}
+              role={"Frontend Lead"}
+              devrole={"FullStack Engineer"}
+              ClassYear={"2025"}
+              Major={"Computer Science"}
+              Email={"lkhirano@wpi.edu"}
+              Github={"https://github.com/notLeoHirano"}
+              Linkdin={"asdf.com"}
+              Imagepath={Leo}
+              Favorite_quote={"One million billion marbles"}
+            />
+            <AboutCard
+              Name={"Vaansh Mansharamani"}
+              role={"Assistant Lead"}
+              devrole={"Algorithm Engineer"}
+              ClassYear={"2026"}
+              Major={"Computer Science"}
+              Email={" vmansharamani@wpi.edu"}
+              Github={"https://github.com/JustSomeOrdinaryCardboardBox"}
+              Linkdin={
+                "https://www.linkedin.com/in/vaansh-mansharamani-30a6b7227/"
+              }
+              Imagepath={Vaanch}
+              Favorite_quote={""}
+            />
+            <AboutCard
+              Name={"Ethan Zhong"}
+              role={"Product Manager"}
+              devrole={"Algorithm Engineer"}
+              ClassYear={"2025"}
+              Major={"Computer Science and Robotics Engineering"}
+              Email={"ezhong@wpi.edu"}
+              Github={"https://github.com/Ethan-Z8"}
+              Linkdin={"https://www.linkedin.com/in/ethan-zhong-76886a21a/"}
+              Imagepath={Ethan}
+              Favorite_quote={
+                "Success is not final, failure is not fatal: it is the courage to continue that counts. - Winston Churchill"
+              }
+            />
+            <AboutCard
+              Name={"Mahit Verma"}
+              role={"Product Owner"}
+              devrole={"Full Stack Engineer"}
+              ClassYear={"2026"}
+              Major={"Computer Science and Data Science"}
+              Email={"mverma@wpi.edu"}
+              Github={"https://github.com/MaVeryo"}
+              Linkdin={"https://www.linkedin.com/in/mahit-verma/"}
+              Imagepath={Mahit}
+              Favorite_quote={""}
+            />
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              paddingTop: "20px",
+              paddingBottom: "10px",
+              justifyContent: "space-between",
+            }}
+          >
+            <AboutCard
+              Name={"Zane Altheimer"}
+              role={"Document Analyst"}
+              devrole={"Frontend Engineer"}
+              ClassYear={"2024"}
+              Major={"Robotics Engineering"}
+              Email={"zaaltheimer@wpi.edu"}
+              Github={"https://github.com/z-altheimer"}
+              Linkdin={"https://www.linkedin.com/in/zaltheimer/"}
+              Imagepath={Zane}
+              Favorite_quote={
+                "'The machine does not isolate man from the great problems of nature but plunges him more deeply into them.' - Antoine de Saint-Exupéry, The Little Prince"
+              }
+            />
+            <AboutCard
+              Name={"Justin Smith"}
+              role={"Full-time Developer"}
+              devrole={"Frontend Engineer"}
+              ClassYear={"2025"}
+              Major={"Computer Science and Game Development"}
+              Email={"jmsmith2@wpi.edu"}
+              Github={"https://github.com/JustinS-23"}
+              Linkdin={"https://www.linkedin.com/in/justin-smith-b61647215/"}
+              Imagepath={Justin}
+              Favorite_quote={"'Float like a butterfly, sting like a bee.'"}
+            />
+            <AboutCard
+              Name={"Maxwell Friedman"}
+              role={"Scrum Master"}
+              devrole={"Frontend Engineer"}
+              ClassYear={"2025"}
+              Major={"Robotics Engineering"}
+              Email={"mlfriedman@wpi.edu"}
+              Github={" https://github.com/Steelplate"}
+              Linkdin={" https://www.linkedin.com/in/friedman-maxwell/"}
+              Imagepath={Max}
+              Favorite_quote={""}
+            />
+            <AboutCard
+              Name={"Shawn Patel"}
+              role={""}
+              devrole={"Backend Engineer"}
+              ClassYear={"2026"}
+              Major={"Computer Science"}
+              Email={"snpatel@wpi.edu"}
+              Github={"https://github.com/shawnp221"}
+              Linkdin={"https://www.linkedin.com/in/shawn-patel-359a562a7/"}
+              Imagepath={Shawn}
+              Favorite_quote={
+                "'Every man must do two things alone; he must do his own believing and his own dying.' - Martin Luther King Jr"
+              }
+            />
+
+            <AboutCard
+              Name={"Aishwarya Silam"}
+              role={""}
+              devrole={"Backend Engineer"}
+              ClassYear={"2026"}
+              Major={"Computer Science"}
+              Email={"asdf@wpi.edu"}
+              Github={"google.com"}
+              Linkdin={"asdf.com"}
+              Imagepath={Aish}
+              Favorite_quote={""}
+            />
+          </div>
+        </div>
+        <h6 style={{ textAlign: "center", marginTop: "20px" }}>
+          ©The Brigham & Women’s Hospital maps and data used in this
+          application are copyrighted and provided for the sole use of
+          educational purposes.
+        </h6>
       </div>
-      <h6 style={{ textAlign: "center", marginTop: "20px" }}>
-        ©The Brigham & Women’s Hospital maps and data used in this application
-        are copyrighted and provided for the sole use of educational purposes.
-      </h6>
     </div>
   );
 };
